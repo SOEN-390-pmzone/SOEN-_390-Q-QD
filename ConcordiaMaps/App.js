1d76db17f815b42b7e6e01de2a79099d7fc8fbf4
Shanvin Luo <shanvinluo@gmail.com>
2025-01-28 03:39:09 -0500

    More cleanup

--- ConcordiaMaps/App.js
+++ ConcordiaMaps/App.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {StyleSheet} from 'react-native';
+// import { Text} from 'react-native';
+
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './screen/HomeScreen';
