4b7d25daf5bd6b3f73338cab00e75e832d6a9e86
Jeremy Vieira <98359007+JeremyVieira@users.noreply.github.com>
2025-02-01 13:12:51 -0500

    Styles and Animation Fix
    Move all styles to a seperate stles.js folder. Fixed animation of NavBar so it always works

--- ConcordiaMaps/components/Header.js
+++ ConcordiaMaps/components/Header.js
@@ -1,43 +1,19 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, View, Text, Image } from 'react-native';
+import React from "react";
+import { SafeAreaView, StyleSheet, View, Text, Image } from "react-native";
+import styles from "../styles";
 
 function Header() {
   return (
     <SafeAreaView>
       <View style={styles.header}>
         <Image
-          source={require('../assets/ConcordiaLogo.png')}
-          style={styles.logo} 
+          source={require("../assets/ConcordiaLogo.png")}
+          style={styles.logo}
         />
         <Text style={styles.headerText}>ConcordiaMaps</Text>
       </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  header: {
-    width: '100%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#912338',
-    padding: 10,
-    position: 'relative',
-    zIndex: 1,  // Ensures the logo appears on top
-  },
-  logo: {
-    width: 50,
-    height: 50,
-    marginLeft: 20, 
-  },
-  headerText: {
-    fontWeight: 'bold',
-    fontFamily: 'Times New Roman',
-    fontSize: 25,
-    color: 'white',
-    marginLeft: 10, 
-  },
-});
 
 export default Header;
--- ConcordiaMaps/screen/HomeScreen.js
+++ ConcordiaMaps/screen/HomeScreen.js
@@ -1,33 +1,23 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
-import NavBar from '../components/NavBar'; 
-import Header from '../components/Header'; 
-import Footer from '../components/Footer'; +
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
+import NavBar from "../components/NavBar";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import styles from "../styles";
 
 function HomeScreen() {
   return (
     <View style={styles.container}>
       {/* Add Header and NavBar in the HomeScreen */}
       <Header />
-      <NavBar />  {/* This is the navigation bar */}
-
+      <NavBar /> {/* This is the navigation bar */}
       {/* Map view */}
       <MapView style={styles.map} />
-
-      
+      {/* Footer */}
+      <Footer />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#912338', 
-  },
-  map: {
-    flex: 1,  
-  },
-});
 
 export default HomeScreen;
