86561ca01073f452674906ee909ff0df5bfea0de
Mohamed Gueye <mohamed.gueye88@gmail.com>
2025-02-02 15:33:58 -0500

    First pin created and working

---
+++ ConcordiaMaps/components/MapMarkers.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Marker, Callout } from "react-native-maps";
+import { Text, View, Image, StyleSheet } from "react-native";
+
+const customMarkerImage = require('../assets/PinLogo.png'); 
+
+const MapMarkers = ({ markers }) => {
+  if (!markers || markers.length === 0) return null;
+
+  return markers.map((marker, index) => (
+    <Marker
+      key={index}
+      coordinate={marker.coordinate}
+      title={marker.name}
+    >
+      <Image 
+        source={customMarkerImage}
+        style={styles.markerImage}
+      />
+      <Callout>
+        <View style={styles.calloutContainer}>
+          <Text style={styles.calloutText}>{marker.name}</Text>
+        </View>
+      </Callout>
+    </Marker>
+  ));
+};
+
+const styles = StyleSheet.create({
+  markerImage: {
+    width: 40,  
+    height: 40,
+  },
+  // Style for the Callout container
+  calloutContainer: {
+    width: 150, 
+    height: 60, 
+    padding: 5,
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    borderRadius: 8, 
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  // Style for the text inside the Callout
+  calloutText: {
+    fontSize: 14, 
+    color: '#333', 
+    textAlign: 'center',
+  },
+});
+
+export default MapMarkers;
---
+++ ConcordiaMaps/data/markersData.js
@@ -0,0 +1,39 @@
+export const Building = [
+  {
+    name: "Henry F. Hall",
+    coordinate: {
+      latitude: 45.4973129,
+      longitude: -73.578876,
+    },
+  },
+  {
+    name: "Webster Library",
+    coordinate: {
+      latitude: 45.4968158,
+      longitude: -73.5779337,
+    },
+  },
+  {
+    name: "EV Building",
+    coordinate: {
+      latitude: 45.495538,
+      longitude: -73.577779,
+    },
+  },
+  {
+    name: "John Molson School Of Business",
+    coordinate: {
+      latitude: 45.495309,
+      longitude: -73.579023,
+    },
+  },
+  {
+    name: "John Molson School Of Business",
+    coordinate: {
+      latitude: 45.495278,
+      longitude: -73.578963,
+    },
+  },
+
+];
+
