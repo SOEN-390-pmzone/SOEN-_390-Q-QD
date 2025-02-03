551283223074f92c4941dedb077d2a5e390ca1a0
Mohamed Gueye <mohamed.gueye88@gmail.com>
2025-02-02 19:00:28 -0500

    SGW Campus pin done

--- ConcordiaMaps/data/markersData.js
+++ ConcordiaMaps/data/markersData.js
@@ -1,16 +1,60 @@
 export const Building = [
-  {
-    name: "Henry F. Hall",
-    coordinate: {
-      latitude: 45.4973129,
-      longitude: -73.578876,
-    },
-  },
+
+  // SGW Campus buildings latitude and longitude
+
   {
     name: "Webster Library",
     coordinate: {
       latitude: 45.4968158,
       longitude: -73.5779337,
+    },
+  },
+  {
+    name: "B Annex",
+    coordinate: {
+      latitude: 45.49791,
+      longitude: -73.579441
+      ,
+    },
+  },
+  {
+    name: "CL Annex",
+    coordinate: {
+      latitude: 45.494228,
+      longitude: -73.579299
+      ,
+    },
+  },
+  {
+    name: "CI Annex",
+    coordinate: {
+      latitude: 45.497438,
+      longitude: -73.579946
+      ,
+    },
+  },
+  {
+    name: "D Annex",
+    coordinate: {
+      latitude: 45.497834,
+      longitude: -73.579282
+      ,
+    },
+  },
+  {
+    name: "EN Annex",
+    coordinate: {
+      latitude: 45.496864,
+      longitude: -73.579561
+      ,
+    },
+  },
+  {
+    name: "ER Building",
+    coordinate: {
+      latitude: 45.496362,
+      longitude: -73.580212
+      ,
     },
   },
   {
@@ -21,6 +65,97 @@ export const Building = [
     },
   },
   {
+    name: "FA Annex",
+    coordinate: {
+      latitude: 45.496826,
+      longitude: -73.579481
+      ,
+    },
+  },
+  {
+    name: "Faubourg Tower",
+    coordinate: {
+      latitude: 45.494683,
+      longitude: -73.577659
+      ,
+    },
+  },
+  {
+    name: "Faubourg Building",
+    coordinate: {
+      latitude: 45.494362,
+      longitude: -73.578346
+      ,
+    },
+  },
+  {
+    name: "Grey Nuns Annex",
+    coordinate: {
+      latitude: 45.494071,
+      longitude: -73.577986
+      ,
+    },
+  },
+  {
+    name: "Guy-De Maisonneuve Building",
+    coordinate: {
+      latitude: 45.495682,
+      longitude: -73.578795
+      ,
+    },
+  },
+  {
+    name: "Grey Nuns Building - Main Entrance",
+    coordinate: {
+      latitude: 45.4935,
+      longitude: -73.576873
+      ,
+    },
+  },
+  {
+    name: "GS Building",
+    coordinate: {
+      latitude: 45.496331,
+      longitude: -73.580436
+      ,
+    },
+  },
+  {
+    name: "Henry F. Hall",
+    coordinate: {
+      latitude: 45.4973129,
+      longitude: -73.578876,
+    },
+  },
+  {
+    name: "K Annex",
+    coordinate: {
+      latitude: 45.497803,
+      longitude: -73.579357,
+    },
+  },
+  {
+    name: "LD Building",
+    coordinate: {
+      latitude: 45.496794,
+      longitude: -73.577231,
+    },
+  },
+  {
+    name: "Learning Square",
+    coordinate: {
+      latitude: 45.4963149,
+      longitude: -73.579472,
+    },
+  },
+  {
+    name: "M Annex",
+    coordinate: {
+      latitude: 45.497485,
+      longitude: -73.579953,
+    },
+  },
+  {
     name: "John Molson School Of Business",
     coordinate: {
       latitude: 45.495309,
@@ -28,12 +163,106 @@ export const Building = [
     },
   },
   {
-    name: "John Molson School Of Business",
+    name: "MI Annex",
     coordinate: {
-      latitude: 45.495278,
-      longitude: -73.578963,
+      latitude: 45.49779,
+      longitude: -73.579234,
     },
   },
+  {
+    name: "MU Annex",
+    coordinate: {
+      latitude: 45.497896,
+      longitude: -73.579553,
+    },
+  },
+  {
+    name: "P Annex",
+    coordinate: {
+      latitude: 45.497001,
+      longitude: -73.57988,
+    },
+  },
+  {
+    name: "PR Annex",
+    coordinate: {
+      latitude: 45.49697,
+      longitude: -73.579851,
+    },
+  },
+  {
+    name: "Q Annex",
+    coordinate: {
+      latitude: 45.496619,
+      longitude: -73.579122,
+    },
+  },
+  {
+    name: "R Annex",
+    coordinate: {
+      latitude: 45.496771,
+      longitude: -73.579424,
+    },
+  },
+  {
+    name: "RR Annex",
+    coordinate: {
+      latitude: 45.497407,
+      longitude: -73.579848,
+    },
+  },
+  {
+    name: "Toronto-Dominion Building",
+    coordinate: {
+      latitude: 45.494993,
+      longitude: -73.578129,
+    },
+  },
+  {
+    name: "V Annex",
+    coordinate: {
+      latitude: 45.497021,
+      longitude: -73.579937,
+    },
+  },
+  {
+    name: "Visual Art Building",
+    coordinate: {
+      latitude: 45.495525,
+      longitude: -73.573794,
+    },
+  },
+  {
+    name: "X Annex",
+    coordinate: {
+      latitude: 45.497119,
+      longitude: -73.579937,
+    },
+  },
+  {
+    name: "Z Annex",
+    coordinate: {
+      latitude: 45.497177,
+      longitude: -73.580328,
+    },
+  },
+
+    // Loyola Campus buildings latitude and longitude
+
+    {
+      name: "Administration Building",
+      coordinate: {
+        latitude: 45.458163,
+        longitude: -73.639977,
+      },
+    },
+    {
+      name: "BB Annex",
+      coordinate: {
+        latitude: 45.459849,
+        longitude: -73.639339,
+      },
+    },
 
 ];
 
--- ConcordiaMaps/screen/HomeScreen.js
+++ ConcordiaMaps/screen/HomeScreen.js
@@ -5,6 +5,8 @@ import NavBar from "../components/NavBar";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import styles from "../styles";
+import MapMarkers from "../components/MapMarkers";
+import { Building } from "../data/markersData";
 
 function HomeScreen() {
   return (
@@ -13,7 +15,13 @@ function HomeScreen() {
       <Header />
       <NavBar /> {/* This is the navigation bar */}
       {/* Map view */}
-      <MapView style={styles.map} />
+      <MapView
+        style={styles.map}
+        showsUserLocation={true}
+        loadingEnabled={true}
+      >
+        <MapMarkers markers={Building} />
+      </MapView>
       {/* Footer */}
       <Footer />
     </View>
