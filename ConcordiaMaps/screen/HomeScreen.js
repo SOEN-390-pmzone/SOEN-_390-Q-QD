ed794567185dbd3bebbc900caf25d1f7d4fab94f
Mohamed Gueye <mohamed.gueye88@gmail.com>
2025-02-07 19:13:59 -0500

    merge conflict

--- ConcordiaMaps/screen/HomeScreen.js
+++ ConcordiaMaps/screen/HomeScreen.js
@@ -10,21 +10,48 @@ import ShuttleStop from "../components/ShuttleStop";
 function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Header />
-      <NavBar />
-      {/* This is the navigation bar */}
-      {/* Map view */}
+    <Header />
+    <NavBar />
+    <MapView
+      style={styles.map}
+      region={
+        location
+          ? {
+              latitude: location.latitude,
+              longitude: location.longitude,
+              latitudeDelta: 0.005,
+              longitudeDelta: 0.005,
+            }
+          : {
+              latitude: 45.4973, // Default center (SGW campus)
+              longitude: -73.5789,
+              latitudeDelta: 0.01,
+              longitudeDelta: 0.01,
+            }
+      }
+      showsUserLocation={true}
+      loadingEnabled={true}
+      watchUserLocation={true}
+    >
+      {Building.map((building, index) => (
+        <Marker
+          key={index}
+          coordinate={building.coordinate}
+          title={building.name}
+        >
+          <Image
+            source={customMarkerImage}
+            style={styles.customMarkerImage}
+          />
+        </Marker>
+      ))}
+      <BuildingColoring />
+      <ShuttleStop />
+    </MapView>
+    <Legend />
 
-      <MapView
-        style={styles.map}
-        showsUserLocation={true}
-        loadingEnabled={true}
-      >
-        <ShuttleStop />
-      </MapView>
-      {/* Footer */}
-      <Footer />
-    </View>
+    <Footer />
+  </View>
   );
 }
 export default HomeScreen;
