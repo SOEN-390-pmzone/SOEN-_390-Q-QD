d0ccfe4fcbfffa5c1de6d0956739fcf9b9300e7e
Mohamed Gueye <mohamed.gueye88@gmail.com>
2025-02-09 15:39:08 -0500

    merge fix

--- ConcordiaMaps/__tests__/HomeScreen.test.js
+++ ConcordiaMaps/__tests__/HomeScreen.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import { render, waitFor } from "@testing-library/react-native";
+import axios from "axios";
 import { NavigationContainer } from "@react-navigation/native";
 import { useNavigation } from "@react-navigation/native";
 import HomeScreen from "../screen/HomeScreen";
@@ -27,6 +28,19 @@ jest.mock("../components/NavBar", () => "NavBar");
 jest.mock("../components/Footer", () => "Footer");
 jest.mock("../components/Legend", () => "Legend");
 
+// Mock axios
+jest.mock("axios");
+
+beforeAll(() => {
+  jest.spyOn(console, 'log').mockImplementation(() => {});
+  jest.spyOn(console, 'error').mockImplementation(() => {});
+});
+
+afterAll(() => {
+  console.log.mockRestore();
+  console.error.mockRestore();
+});
+
 describe("HomeScreen", () => {
   it("renders without crashing", async () => {
     useNavigation.mockReturnValue({ navigate: jest.fn() });
@@ -39,5 +53,41 @@ describe("HomeScreen", () => {
 
     await waitFor(() => expect(getByTestId("home-screen")).toBeTruthy());
   });
+
+  it("handles no results found", async () => {
+    const mockResponse = {
+      data: {
+        results: [],
+        status: "ZERO_RESULTS",
+      },
+    };
+
+    axios.get.mockResolvedValueOnce(mockResponse);
+
+    const { getByTestId } = render(
+      <NavigationContainer>
+        <HomeScreen />
+      </NavigationContainer>,
+    );
+
+    await waitFor(() => {
+      expect(getByTestId("error-message").props.children).toBe("ZERO_RESULTS");
+    });
+  });
+
+  it("handles API errors", async () => {
+    axios.get.mockRejectedValueOnce(new Error("Network Error"));
+
+    const { getByTestId } = render(
+      <NavigationContainer>
+        <HomeScreen />
+      </NavigationContainer>,
+    );
+
+    await waitFor(() => {
+      expect(getByTestId("error-message").props.children).toBe(
+        "Something went wrong. Please try again later.",
+      );
+    });
+  });
 });
-//
