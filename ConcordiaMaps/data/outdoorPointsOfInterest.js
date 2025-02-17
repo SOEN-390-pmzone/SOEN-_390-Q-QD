cde6cf5f4f7b9df0e093730fc98020b3c54f9146
Shanvin Luo <shanvinluo@gmail.com>
2025-02-16 21:26:36 -0500

    Update outdoorPointsOfInterest.js

--- ConcordiaMaps/data/outdoorPointsOfInterest.js
+++ ConcordiaMaps/data/outdoorPointsOfInterest.js
@@ -6,6 +6,8 @@ export const OutdoorPtInterest = [
     coordinate: {
       latitude: 45.4968158,
       longitude: -73.5779337,
+    description: "Coffee Shop",
+    address: "1455 De Maisonneuve Blvd W, Montreal, QC H3G 1M8",
     },
   },
 ];
