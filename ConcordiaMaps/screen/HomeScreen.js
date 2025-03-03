6bde5d6a17c49e87b4b2d1a7c7df33b605080a05
Atai Askarov <101080966+Atai-Askarov@users.noreply.github.com>
2025-03-03 15:44:09 -0500

    Major changes to Home Screen + additives
    The modal has been created
    The image for the Toggle has been changed to indicate campus switch
    ConvertToCoordinates has been rewritten as a component
    AsyncStorage has been implemented to store the last seen campus

--- ConcordiaMaps/screen/HomeScreen.js
+++ ConcordiaMaps/screen/HomeScreen.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef, useContext } from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
-import axios from "axios";
 import MapView, { Marker } from "react-native-maps";
 import NavBar from "../components/NavBar";
 import Header from "../components/Header";
@@ -8,78 +7,57 @@ import TemporaryModal from "../components/temporaryModal";
 import { LocationContext } from "../contexts/LocationContext";
 import Footer from "../components/Footer";
 import styles from "../styles";
-
 const customMarkerImage = require("../assets/PinLogo.png");
 import { Building } from "../components/MapMarkers";
 import BuildingColoring from "../components/buildingColoring";
 import Legend from "../components/Legend";
 import ShuttleStop from "../components/ShuttleStop";
+import { saveToAsyncStorage, getFromAsyncStorage } from "../components/AsyncPersistence";
+import convertToCoordinates from "../components/convertToCoordinates";
 
-function HomeScreen() {
+function HomeScreen({ asyncKey = "Campus" }) {
   const loyolaPostalCode = process.env.EXPO_PUBLIC_LOYOLA_POSTAL_CODE;
   const sgwPostalCode = process.env.EXPO_PUBLIC_SGW_POSTAL_CODE;
-  const GOOGLE_MAPS_API_KEY = process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY;
 
   const location = useContext(LocationContext);
 
-  const [postalCode, setPostalCode] = useState(sgwPostalCode);
+  const [postalCode, setPostalCode] = useState(null);
   const [coordinates, setCoordinates] = useState(null);
   const [error, setError] = useState("");
   const [modalState, setModalState] = useState(true);
-  const [borderColor, setBorderColor] = useState("#912338"); // Initial border color (black)
-
+  const [borderColor, setBorderColor] = useState("#912338"); // Initial border color (red)
   const mapRef = useRef(null);
 
-  const convertToCoordinates = async (postal_code) => {
-    try {
-      const response = await axios.get(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${postal_code}&key=${GOOGLE_MAPS_API_KEY}`,
-      );
-      const { status, results } = response.data;
 
-      if (status === "OK") {
-        if (results.length > 0) {
-          const { lat, lng } = results[0].geometry.location;
-          setCoordinates({ latitude: lat, longitude: lng });
-          setError("");
-        } else {
-          setCoordinates(null);
-          setError("No results found.");
-        }
+  useEffect(() => {
+    const fetchLastCampus = async () => {
+      const campus = await getFromAsyncStorage(asyncKey, sgwPostalCode);
+      setPostalCode(campus);
+      const coords = await convertToCoordinates(campus);
+      if (coords.error) {
+        setError(coords.error);
       } else {
-        setCoordinates(null);
-        setError(`${status}`);
+        setCoordinates(coords);
       }
-    } catch (error) {
-      setCoordinates(null);
-      setError("Something went wrong. Please try again later.");
-      console.log(error);
-    }
   };
+  fetchLastCampus();
+  },[])
 
   useEffect(() => {
-    if (coordinates && mapRef.current) {
-      mapRef.current.animateToRegion(
-        {
-          latitude: coordinates.latitude,
-          longitude: coordinates.longitude,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
-        },
-        2500,
-      ); // Duration of the animation in milliseconds
-    }
-  }, [coordinates]);
-
-  useEffect(() => {
-    convertToCoordinates(postalCode);
+    const saveCurrentCampus = async(asyncKey, postalcode) =>{
+      if (postalcode) {
+        await saveToAsyncStorage(asyncKey, postalCode);
+        const coordinates = await convertToCoordinates(postalCode);
+        if (coordinates.error) {
+          setError(coordinates.error);
+        } else {
+          setCoordinates(coordinates);
+        }
+      }
+      }
+    saveCurrentCampus(asyncKey, postalCode);
   }, [postalCode]);
-
-  const handleChangeCampuses = () => {
-    setPostalCode((prevPostalCode) =>
-      prevPostalCode === sgwPostalCode ? loyolaPostalCode : sgwPostalCode,
-    );
-  };
+  
 
   useEffect(() => {
     if (modalState) {
@@ -92,6 +70,26 @@ function HomeScreen() {
     }
   }, [modalState]);
 
+  useEffect(() => {
+    if (coordinates && mapRef.current) {
+
+      mapRef.current.animateToRegion(
+        {
+          latitude: coordinates.latitude,
+          longitude: coordinates.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        },
+        2500,
+      ); // Duration of the animation in milliseconds
+    }
+  }, [coordinates]);
+
+  const handleChangeCampuses = () => {
+    setPostalCode((prevPostalCode) =>
+      prevPostalCode === sgwPostalCode ? loyolaPostalCode : sgwPostalCode,
+    );
+  };
   return (
     <View style={styles.container}>
       <Header />
@@ -100,6 +98,11 @@ function HomeScreen() {
 
       {coordinates ? (
         <>
+          <TemporaryModal
+          text="Press the button to switch campuses"
+          my_state={modalState}
+          time="3000"
+        />
           <MapView
             testID="map-view"
             style={styles.map}
@@ -113,8 +116,8 @@ function HomeScreen() {
                     longitudeDelta: 0.005,
                   }
                 : {
-                    latitude: 45.4973, // Default center (SGW campus)
-                    longitude: -73.5789,
+                    latitude: coordinates.latitude, // Default center (SGW campus)
+                    longitude: coordinates.longitude,
                     latitudeDelta: 0.01,
                     longitudeDelta: 0.01,
                   }
@@ -138,32 +141,31 @@ function HomeScreen() {
             <BuildingColoring />
             <ShuttleStop />
           </MapView>
+          <View style={styles.toggleView}>
+          <TouchableOpacity
+            testID="toggle-button"
+            onPress={handleChangeCampuses}
+            activeOpacity={0.7}
+            style={{ borderColor: borderColor, borderWidth: 2, borderRadius: 10 }}
+          >
+            <Image
+              style={styles.buttonImage}
+              source={require("../assets/ToggleButton.png")}
+              resizeMode={"cover"} // cover or contain its up to you view look
+            />
+          </TouchableOpacity>
+      </View>
+      <Legend />
         </>
       ) : (
         <Text>Loading...</Text>
       )}
       {error ? <Text>Error: {error}</Text> : null}
-      <View style={styles.toggleView}>
-        <TouchableOpacity
-          onPress={handleChangeCampuses}
-          activeOpacity={0.7}
-          style={{ borderColor: borderColor, borderWidth: 2, borderRadius: 10 }}
-        >
-          <Image
-            style={styles.buttonImage}
-            source={require("../assets/download.jpg")}
-            resizeMode={"cover"} // cover or contain its up to you view look
-          />
-        </TouchableOpacity>
-        <TemporaryModal
-          text="Press the coat of arms to switch campuses"
-          my_state={modalState}
-          time="3000"
-        />
-      </View>
+      
 
-      <Legend />
+      
       <Footer />
+      
     </View>
   );
 }
