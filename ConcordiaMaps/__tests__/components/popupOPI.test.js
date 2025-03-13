d3695c7facf0476eea053f45c18ebd2403fdad34
Shanvin Luo <shanvinluo@gmail.com>
2025-03-13 05:18:43 -0400

    prettier

--- ConcordiaMaps/__tests__/components/popupOPI.test.js
+++ ConcordiaMaps/__tests__/components/popupOPI.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react-native";
-import PopupOPI from "../../components/PopupOPI"; 
+import PopupOPI from "../../components/PopupOPI";
 import "@testing-library/jest-native/extend-expect";
 
 describe("<PopupOPI/>", () => {
