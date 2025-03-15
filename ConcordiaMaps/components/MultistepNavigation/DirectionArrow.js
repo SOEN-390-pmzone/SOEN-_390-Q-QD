e81f7ff92a8b4d35d6fc6da5e1a35a5c27fd1d16
David R <99555747+GitExcited@users.noreply.github.com>
2025-03-15 14:43:25 -0400

    REFACTOR : "Move Function" to relocate styleSheet to a better location

--- ConcordiaMaps/components/MultistepNavigation/DirectionArrow.js
+++ ConcordiaMaps/components/MultistepNavigation/DirectionArrow.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import styles from "../../styles/MultistepNavigation/DirectionArrowStyles"
 
 const DirectionArrow = () => (
   <View style={styles.container}>
     <Ionicons name="chevron-down" size={28} color="#912338" style={styles.icon} />
   </View>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    paddingVertical: 8,
-  },
-  icon: {
-    textShadowColor: 'rgba(0, 0, 0, 0.2)',
-    textShadowOffset: { width: 1, height: 1 },
-    textShadowRadius: 1,
-  }
-});
 
 export default DirectionArrow;
