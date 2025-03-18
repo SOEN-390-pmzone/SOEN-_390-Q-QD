2b6be4b1b543b878d3d43895214c54b4b0065523
Atai Askarov <101080966+Atai-Askarov@users.noreply.github.com>
2025-03-18 15:43:48 -0400

    ran prettier . --write

--- ConcordiaMaps/constants/coordinates/CampusCoordinates.js
+++ ConcordiaMaps/constants/coordinates/CampusCoordinates.js
@@ -1,14 +1,14 @@
 export const CampusCoordinates = {
-    SGW: {
-      latitude: 45.4973,
-      longitude: -73.5789,
-      latitudeDelta: 0.01,
-      longitudeDelta: 0.01,
-    },
-    Loyola: {
-      latitude: 45.458256,
-      longitude: -73.640472,
-      latitudeDelta: 0.01,
-      longitudeDelta: 0.01,
-    },
-  };
+  SGW: {
+    latitude: 45.4973,
+    longitude: -73.5789,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  },
+  Loyola: {
+    latitude: 45.458256,
+    longitude: -73.640472,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  },
+};
